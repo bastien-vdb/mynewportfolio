f049fac2470618e1a38448c81e30d33dbe12bfdd
bastien-vdb <bastien.deboisrolin@gmail.com>
2023-05-02 14:44:28 +0200

    css added

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -2,8 +2,6 @@ import Proj1 from "../assets/proj1.jpg";
 import Proj2 from "../assets/proj2.jpg";
 import Proj3 from "../assets/proj3.jpg";
 import Proj4 from "../assets/proj4.jpg";
-import Proj5 from "../assets/proj5.jpg";
-import Proj6 from "../assets/proj6.png";
 
 export const ProjectList = [
   {
@@ -29,15 +27,5 @@ export const ProjectList = [
     image: Proj4,
     skills: "Three JS, React, TailwindCSS, Web3, Hardhat, Ethers.js",
     link: "https://dbrolinconsulting.com/Start"
-  },
-  {
-    name: "Dashboard Visualizer",
-    image: Proj5,
-    skills: "JavaScript,HTML,CSS",
-  },
-  {
-    name: "Mobile Game",
-    image: Proj6,
-    skills: "React Native,JavaScript,HTML,CSS",
   },
 ];
