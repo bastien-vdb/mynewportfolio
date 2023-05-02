98ebe8abf1adf825a5ea7b90983c6af4e878d832
bastien-vdb <bastien.deboisrolin@gmail.com>
2023-05-02 16:14:16 +0200

    typescript error fixing

--- src/pages/projects/[id]/index.js
+++ src/pages/projects/[id]/index.js
@@ -11,11 +11,11 @@ function ProjectDisplay() {
   const project = ProjectList[id];
   return (
     <div className="project">
-      <h1> {project.name}</h1>
-      <Image className="" src={project.image} height={1651} width={957} alt={project.name} />
+      <h1> {project?.name}</h1>
+      <Image className="" src={project?.image} height={1651} width={957} alt={project?.name} />
       <Link className="text-4xl text-blue-950 py-6 hover:text-sky-800 hover:text-3xl duration-700 font-bold" href={project.link ? project.link : ''}>See more ...</Link>
       <p>
-        <b>Skills:</b> {project.skills}
+        <b>Skills:</b> {project?.skills}
       </p>
       <GitHubIcon />
     </div>
