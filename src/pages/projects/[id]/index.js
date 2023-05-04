c508a847091fb20fe487d143ede0dca728b9955e
bastien-vdb <bastien.deboisrolin@gmail.com>
2023-05-04 12:37:10 +0200

    fix mobile version6

--- src/pages/projects/[id]/index.js
+++ src/pages/projects/[id]/index.js
@@ -10,14 +10,18 @@ function ProjectDisplay() {
 
   const project = ProjectList[id];
   return (
-    <div className="project"> 
+    <div className="project">
       <h1> {project?.name}</h1>
       <Image className="" src={project?.image} height={1651} width={957} alt={project?.name} />
-      <Link className="text-3xl text-blue-950 py-6 hover:text-sky-800 hover:text-3xl duration-700 font-bold" target="_blank" href={project?.link ? project?.link : ''}>See more ...</Link>
+      <Link className="text-3xl text-blue-950 py-6 hover:text-sky-800 hover:text-3xl duration-700 font-bold" target="_blank" href={project?.link ? project?.link : ""}>
+        See more ...
+      </Link>
       <p>
         <b>Skills:</b> {project?.skills}
       </p>
-      <GitHubIcon />
+      <Link href={project?.git ? project?.git : ""} target="_blank">
+        <GitHubIcon />
+      </Link>
     </div>
   );
 }
